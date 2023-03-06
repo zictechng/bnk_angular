c48717a93fd664299fea9313460eb05d5fc4e26b
angular_ecom <zictechng@gmail.com>
2023-03-06 09:54:56 +0000

    update search api

---
+++ src/app/components/dashboard/buy_product.ts
@@ -0,0 +1,136 @@
+[{
+  "_id": {
+    "$oid": "63ff374b2548d2ce6697462c"
+  },
+  "product_name": "T-Shit1 ",
+  "product_description": "White shirt",
+  "product_category": "Secondary",
+  "product_cost_price": 100,
+  "product_sale_price": 120,
+  "product_qty": 40,
+  "product_code_number": "20202021",
+  "product_expiration_date": "2023-03-25",
+  "reg_code": "oRhzv6uISbL7m2ig4YubEhR3l",
+  "addedby": "63a82717823370c509263dd0",
+  "product_status": "Pending",
+  "createdOn": {
+    "$date": {
+      "$numberLong": "1677670219649"
+    }
+  },
+  "__v": 0
+},{
+  "_id": {
+    "$oid": "63ff38542e02ed3b888c0d26"
+  },
+  "product_name": "T-Shit 2",
+  "product_description": "Black Sleve Shirt",
+  "product_category": "KG",
+  "product_cost_price": 200,
+  "product_sale_price": 220,
+  "product_qty": 50,
+  "product_code_number": "20202026",
+  "product_expiration_date": "2024-02-01",
+  "product_buy_date": {
+    "$date": {
+      "$numberLong": "1675123200000"
+    }
+  },
+  "reg_code": "MrMd4uRvreJWeei5KTWkpq8gh",
+  "addedby": "63a82717823370c509263dd0",
+  "product_status": "Pending",
+  "createdOn": {
+    "$date": {
+      "$numberLong": "1677670484324"
+    }
+  },
+  "__v": 0
+},{
+  "_id": {
+    "$oid": "63ff39a54b135e34fe97e049"
+  },
+  "product_name": "T-Shit 4",
+  "product_description": "Black Sleve Shirt",
+  "product_category": "KG",
+  "product_cost_price": 100,
+  "product_sale_price": 110,
+  "product_qty": 35,
+  "product_code_number": "20202023",
+  "product_expiration_date": "2023-03-16",
+  "product_buy_date": "2023-02-28",
+  "reg_code": "XTn0YcYpFHE2Bvjn75nrjb85Y",
+  "addedby": "63a82717823370c509263dd0",
+  "product_status": "Pending",
+  "createdOn": {
+    "$date": {
+      "$numberLong": "1677670821915"
+    }
+  },
+  "__v": 0
+},{
+  "_id": {
+    "$oid": "63ff3a474b135e34fe97e04f"
+  },
+  "product_name": "CWay Water",
+  "product_description": "Cway bolt water",
+  "product_category": "Nursery",
+  "product_cost_price": 80,
+  "product_sale_price": 100,
+  "product_qty": 200,
+  "product_code_number": "156000261201",
+  "product_expiration_date": "2023-03-31",
+  "product_buy_date": "2023-03-13",
+  "reg_code": "jIEzsRbtp0LgxM4YxOlXkVgOC",
+  "addedby": "63a82717823370c509263dd0",
+  "product_status": "Pending",
+  "createdOn": {
+    "$date": {
+      "$numberLong": "1677670983873"
+    }
+  },
+  "__v": 0
+},{
+  "_id": {
+    "$oid": "63ff3c474b135e34fe97e065"
+  },
+  "product_name": "Iphone 7",
+  "product_description": "Black Iphone 7",
+  "product_category": "Pre-Nursery",
+  "product_cost_price": 20000,
+  "product_sale_price": 25000,
+  "product_qty": 10,
+  "product_code_number": "9900221",
+  "product_expiration_date": "2023-03-29",
+  "product_buy_date": "2023-03-06",
+  "reg_code": "ycdKolFiPgEViY5WeKFsGbXUJ",
+  "addedby": "63a82717823370c509263dd0",
+  "product_status": "Pending",
+  "createdOn": {
+    "$date": {
+      "$numberLong": "1677671495174"
+    }
+  },
+  "__v": 0
+},{
+  "_id": {
+    "$oid": "63ff3e213095bc693ccfed79"
+  },
+  "product_name": "Power Bank",
+  "product_description": "A new black model power bank",
+  "product_category": "Pre-KG",
+  "product_cost_price": 5000,
+  "product_sale_price": 6000,
+  "product_qty": 5,
+  "product_code_number": "pw3044009",
+  "product_expiration_date": "2023-03-29",
+  "product_buy_date": "2023-03-06",
+  "reg_code": "DPlanZG9dDAvl3P5LGkHmNgOP",
+  "addedby": "63a82717823370c509263dd0",
+  "product_status": "Pending",
+  "createdOn": {
+    "$date": {
+      "$numberLong": "1677671969991"
+    }
+  },
+  "__v": 0
+}]
